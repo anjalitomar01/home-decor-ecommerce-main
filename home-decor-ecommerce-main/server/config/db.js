454ba4ccbc3b872115c203109b91afd5616248b0
rjagd <rjagd@HP>
2025-04-27 23:20:32 +0530

    db update

--- home-decor-ecommerce-main/server/config/db.js
+++ home-decor-ecommerce-main/server/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-// const DB = "mongodb+srv://rajputrashmi321:FIlPR4P8qgaPGQ5X@cluster0home.qtm1cyj.mongodb.net/newhomedecore?retryWrites=true&w=majority&appName=Cluster0Home";
-const DB = "mongodb+srv://anjalithakur96505:S4Atr4LAbKPjPxrO@cluster0.dzgdfbb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+ const DB = "mongodb+srv://rajputrashmi321:FIlPR4P8qgaPGQ5X@cluster0home.qtm1cyj.mongodb.net/newhomedecore?retryWrites=true&w=majority&appName=Cluster0Home";
+//const DB = "mongodb+srv://anjalithakur96505:S4Atr4LAbKPjPxrO@cluster0.dzgdfbb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const connectDB = async () => {
   try {
     await mongoose.connect(DB, {
